3e7a98bd392a46fb02eaca5ae14152082cce0415
Miguel Quintana <miguelquintanadev@gmail.com>
2024-05-05 00:24:41 -0400

    connect

---
+++ src/test/java/com/miguel/springboot/error/springbooterror/SpringbootErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.springboot.error.springbooterror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
